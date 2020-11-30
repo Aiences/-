a3f5a92b4d474a8415f58848a816694ea35cc8dc
wwjapple <wwjapple@foxmail.com>
2020-11-30 14:59:06 +0800

    封装input 校验组件

---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <input
+      class="form-control"
+      :value="inputRef.value"
+      @input="updateValue"
+      v-model="inputRef.value"
+      @blur="validateInput"
+      v-bind="$attrs"
+    />
+    <small class="my-remaind" v-if="inputRef.error">{{
+      inputRef.message
+    }}</small>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType, reactive, Ref, ref, toRefs } from "vue";
+
+interface RuleProp {
+  type: "required" | "email"; //可配置
+  message: string;
+}
+const pattern = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/; //校验邮箱规则
+
+export type RulesProp = RuleProp[];
+
+export default defineComponent({
+  name: "ValidateInput",
+  inheritAttrs: false, //不希望组件的根元素继承attribute
+  props: {
+    rules: Array as PropType<RulesProp>,
+    modelValue: String,
+  },
+  setup(props, context) {
+    console.log(context.attrs);
+    const inputRef = reactive({
+      value: props.modelValue || "",
+      error: false,
+      message: "",
+    });
+
+    //实现input数据双向绑定
+    const updateValue = (e: KeyboardEvent) => {
+      const textValue = (e.target as HTMLInputElement).value;
+      inputRef.value = textValue;
+      context.emit("update:modelValue", textValue);
+    };
+
+    const validateInput = () => {
+      if (props.rules) {
+        const allPassed = props.rules.every((rule) => {
+          let pass = true;
+          inputRef.message = rule.message;
+          switch (rule.type) {
+            case "required":
+              pass = inputRef.value.trim() !== "";
+              break;
+            case "email":
+              pass = pattern.test(inputRef.value);
+              break;
+
+            default:
+              break;
+          }
+          return pass;
+        });
+        inputRef.error = !allPassed;
+      }
+    };
+
+    return {
+      inputRef,
+      validateInput,
+      updateValue,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.my-remaind {
+  color: red;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">email</label>
+        <validate-input
+          :rules="rules"
+          v-model="initValue"
+          type="text"
+          class="input-box"
+        ></validate-input>
+      </div>
+
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import ValidateInput, { RulesProp } from "@/components/ValidateInput.vue";
+import { defineComponent, reactive, ref } from "vue";
+
+export default defineComponent({
+  components: { ValidateInput },
+  name: "Login",
+  setup() {
+    const initValue = ref("test");
+    const rules: RulesProp = [
+      {
+        type: "required",
+        message: "请输入邮箱",
+      },
+      {
+        type: "email",
+        message: "请输入正确的邮箱地址",
+      },
+    ];
+
+    return {
+      rules,
+      initValue,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.input-box {
+  border-radius: 4px;
+}
+</style>
